fb09b10b4682239762a428f77e81fdfb434399a3
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-09-13 22:21:41 +0300

    Init app

---
+++ src/main/java/com/nmuzychuk/store/product/ProductController.java
@@ -0,0 +1,32 @@
+package com.nmuzychuk.store.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("")
+    public @ResponseBody
+    Iterable<Product> addProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping("")
+    public @ResponseBody
+    Product addProducts(@RequestParam String name) {
+        Product product = new Product();
+        product.setName(name);
+        product = productRepository.save(product);
+        return product;
+    }
+}
---
+++ src/main/java/com/nmuzychuk/store/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.nmuzychuk.store.product;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
