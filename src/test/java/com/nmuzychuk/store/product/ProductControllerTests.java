bd982033db08427ba61eebb16f19205aa16832db
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-09-24 21:42:10 +0300

    Add product controller test

---
+++ src/test/java/com/nmuzychuk/store/product/ProductControllerTests.java
@@ -0,0 +1,40 @@
+package com.nmuzychuk.store.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ProductControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Test
+    public void testShowProductList() throws Exception {
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddProduct() throws Exception {
+        mockMvc.perform(post("/products")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "foo"))
+                .andExpect(status().isOk());
+    }
+}
