f29645ce51561c82909918daef0d53c6ca59998a
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-11-15 22:44:32 +0200

    Disable security in tests

--- src/test/java/com/nmuzychuk/store/admin/product/ProductControllerTests.java
+++ src/test/java/com/nmuzychuk/store/admin/product/ProductControllerTests.java
@@ -4,6 +4,7 @@ import com.nmuzychuk.store.product.ProductRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@AutoConfigureMockMvc(secure = false)
 public class ProductControllerTests {
 
     @Autowired
