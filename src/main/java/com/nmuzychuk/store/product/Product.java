e06a3cac9fbcd0b029d7cb38d8e96d8591f3acb7
Nazarii Muzychuk <muzychuk.n@gmail.com>
2019-01-03 20:46:35 +0200

    Add product price

--- src/main/java/com/nmuzychuk/store/product/Product.java
+++ src/main/java/com/nmuzychuk/store/product/Product.java
@@ -1,10 +1,12 @@
 package com.nmuzychuk.store.product;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
 public class Product {
@@ -13,6 +15,9 @@ public class Product {
     private Integer id;
 
     private String name;
+
+    @NumberFormat(style = NumberFormat.Style.NUMBER, pattern = "#0.00")
+    private BigDecimal price;
 
     public Integer getId() {
         return id;
@@ -28,6 +33,14 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
     }
 
     @Override
